e9750735a61406ff63f1a2448ec7d0cefeedc44e
Ainyaku <87048351+Ainyaku@users.noreply.github.com>
2021-10-29 10:35:36 -0400

    Create share-java-article.js

---
+++ share-java-article.js
@@ -0,0 +1,6 @@
+navigator.share({
+    title: 'Minecraft Java Launcher Gray Screen Fix',
+    text: 'Have you ever tried playing Minecraft Java Edition, but when opening the launcher you were greeted by a gray screen that never goes away? Well here's a simple fix that should only take a few minutes to do.',
+    url: 'https://🔄.ainyaku.com/JavaFix
+',
+  })
